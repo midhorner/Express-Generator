13f087d2980a9aa714b23efa8aef648aa8d26b09
midhorner <horner.andrew.e@gmail.com>
2021-07-07 15:07:53 -0400

    multer middleware and image upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 // connects to mongodb server through mongoose
 const mongoose = require('mongoose');
@@ -29,6 +30,20 @@ connect.then(
 
 const app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get('secPort')}${req.url}`
+    );
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -47,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/partners', partnerRouter);
 app.use('/promotions', promotionRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
